2caf0ff3ba37da6275d0de34160fa2b4bb400200
Djakh <sh.bostihonov@imkon.uz>
2025-03-18 19:32:08 +0500

    Issue #1: details remain

---
+++ lib/view/regions_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:panda/assets/colors/app_colors.dart';
+import 'package:panda/assets/constants/app_images.dart';
+import 'package:panda/assets/style.dart';
+
+class RegionsPage extends StatefulWidget {
+  const RegionsPage({super.key});
+
+  @override
+  State<RegionsPage> createState() => _RegionsPageState();
+}
+
+class _RegionsPageState extends State<RegionsPage> {
+  /// --- Variables ---
+  final List<String> regions = [
+    "Андижанская область",
+    "Бухарская область",
+    "Джизахская область",
+    "Каракалпакстан",
+    "Навоийская область",
+    "Самаркандская область",
+    "Ташкентская область",
+    "Наманганская область",
+    "Ферганская область",
+    "Хорезмская область",
+    "Сурхандарьинская область",
+    "Сырдарьинская область"
+  ];
+
+  List<String> filteredRegions = [];
+
+  /// --- Lifecycle ---
+
+  @override
+  void initState() {
+    super.initState();
+    filteredRegions = regions;
+  }
+
+  /// --- Methods ---
+
+  void _filterRegions(String query) {
+    setState(() {
+      filteredRegions = regions.where((region) => region.toLowerCase().contains(query.toLowerCase())).toList();
+    });
+  }
+
+  /// --- Widgets ---
+  Widget image(String image, {double? height, double? width}) => Image.asset(AppImages.search, height: height ?? 18, width: width ?? 18);
+
+  Widget get searchField => TextField(
+      onChanged: _filterRegions,
+      decoration: InputDecoration(
+          prefixIcon: image(AppImages.search),
+          hintText: "Поиск",
+          hintStyle: Style.small3w4.copyWith(color: AppColors.grey1),
+          filled: true,
+          fillColor: AppColors.grey0n5,
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12.0), borderSide: BorderSide.none)));
+
+  Widget regionItem(String region) => ListTile(
+      title: Text(region, style: Style.bodyw4),
+      trailing: const Icon(Icons.arrow_forward_ios, size: 16),
+      onTap: () => print("Выбрано: $region"));
+
+  Widget get regionList => Expanded(
+      child: ListView.separated(
+          itemCount: filteredRegions.length,
+          separatorBuilder: (_, __) => const Divider(),
+          itemBuilder: (_, index) => regionItem(filteredRegions[index])));
+
+  Widget get header => Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [searchField, const SizedBox(height: 16), Text("Выбрать область", style: Style.bodyw4), const SizedBox(height: 8)]);
+
+  Widget get view => Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16.w),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [const SizedBox(height: 16), header, regionList]));
+
+  Widget get title => Text("Местоположение", style: Style.headlinew7);
+
+  Widget get backButtonAndTitle => Row(
+        children: [IconButton(icon: const Icon(Icons.arrow_back, color: Colors.black), onPressed: () => Navigator.pop(context)), title],
+      );
+
+  AppBar get appbar => AppBar(
+      title: title,
+      centerTitle: false,
+      surfaceTintColor: AppColors.white,
+      backgroundColor: AppColors.white,
+      scrolledUnderElevation: 0,
+      elevation: 0);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(backgroundColor: AppColors.white, appBar: appbar, body: SafeArea(child: view));
+}
